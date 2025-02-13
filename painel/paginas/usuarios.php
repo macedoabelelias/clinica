44b83c2ed7b4cc688baa57b1b356c050803d4617
abelmacedo <macedoabel.elias@gmail.com>
2025-02-13 17:22:32 -0300

    Modal Inserir e máscaras

--- painel/paginas/usuarios.php
+++ painel/paginas/usuarios.php
@@ -2,7 +2,7 @@
 $pag ='usuarios';
 
  ?>
-<a type="button" class="btn btn-primary"><span class="fa fa-plus"></span> Usuário</a>
+<a onclick="inserir()" type="button" class="btn btn-primary"><span class="fa fa-plus"></span> Usuário</a>
 
 <div class="bs-example widget-shadow" style="padding:15px" id="listar">
     
@@ -10,5 +10,109 @@ $pag ='usuarios';
 
 <script type="text/javascript">var pag = "<?=$pag?>"</script>
 <script src="js/ajax.js"></script>
+
+<!-- Modal Perfil -->
+<div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="exampleModalLabel"><span id="titulo_inserir"></span></h4>
+				<button id="btn-fechar" type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-top: -25px">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form id="form">
+			<div class="modal-body">
+				
+
+					<div class="row">
+						<div class="col-md-6">							
+								<label>Nome</label>
+								<input type="text" class="form-control" id="nome" name="nome" 
+                                placeholder="Seu Nome" required>							
+						</div>
+
+						<div class="col-md-6">							
+								<label>Email</label>
+								<input type="email" class="form-control" id="email" name="email" 
+                                placeholder="Seu E-mail" required>							
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="col-md-12">							
+								<label>Endereço</label>
+								<input type="text" class="form-control" id="endereco" name="endereco" 
+                                placeholder="Endereço completo">							
+						</div>
+                    </div>
+
+                    <div class="row">
+						<div class="col-md-6">							
+								<label>Telefone</label>
+								<input type="text" class="form-control" id="telefone" name="telefone" 
+                                placeholder="Seu Telefone" required>							
+						</div>
+                        
+                        <div class="col-md-6">							
+								<label>Nível</label>
+								<select class="form-control" name="nivel" id="nivel">
+                                    <option>Administrador</option>
+                                    <option>Comum</option>
+                                </select>
+						</div>						
+					</div>
+
+
+					<div class="row">
+						<div class="col-md-8">							
+								<label>Foto</label>
+								<input type="file" class="form-control" id="foto" name="foto"                                
+                                onchange="carregarImg()">							
+						</div>
+
+						<div class="col-md-4">								
+							<img src="images/perfil/sem-foto.jpg"  width="80px" id="target">								
+							
+						</div>
+
+						
+					</div>
+
+
+					<input type="hidden" name="id_usuario" value="<?php echo $id_usuario ?>">
+				
+
+				<br>
+				<small><div id="msg-perfil" align="center"></div></small>
+			</div>
+			<div class="modal-footer">       
+				<button type="submit" class="btn btn-primary">Salvar</button>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	function carregarImg() {
+    var target = document.getElementById('target');
+    var file = document.querySelector("#foto").files[0];
+    
+        var reader = new FileReader();
+
+        reader.onloadend = function () {
+            target.src = reader.result;
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+
+        } else {
+            target.src = "";
+        }
+    }
+</script>
 
 
